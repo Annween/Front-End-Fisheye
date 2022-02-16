ca9a23e849687c7cbeb4dff1ca78a213836569c7
Annween <49524082+Annween@users.noreply.github.com>
2022-02-16 17:09:56 +0100

    first commit

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,44 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const  { id, name, city, country, tagline, price, portrait } = data || {};
 
     const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
+    function getUserCardDOM(qualifiedName) {
+        const ahref = document.createElement('a');
+        ahref.setAttribute('class', 'link')
+        ahref.setAttribute('href', 'photographer.html?id='+ id);
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
+        const villePays = document.createElement('span');
+        const bio = document.createElement('p');
+        const prix = document.createElement('div')
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        villePays.textContent = city.concat(', ', country);
+        bio.textContent = tagline;
+        prix.setAttribute("class", "prix");
+        prix.textContent = price + "€ /jour";
+        ahref.appendChild(article);
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(villePays);
+        article.appendChild(bio);
+        article.appendChild(prix);
+        return (ahref);
     }
-    return { name, picture, getUserCardDOM }
+
+    function getProfilePage()
+    {
+           const photograph_header = document.createElement('div');
+            photograph_header.setAttribute("class", "photographer-header");
+            const nomPrenom = document.createElement("h1");
+            nomPrenom.textContent = name;
+            return (photograph_header);
+
+
+    }
+
+    return { id, getUserCardDOM, getProfilePage }
+
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,68 @@
 //Mettre le code JavaScript lié à la page photographer.html
+const getPhotographer = async () => { //get each photographer identify by their id
+    const {photographers} = await (
+        fetch('./data/photographers.json')
+            .then((response) => response.json())
+    );
+
+    const id = window.location.search.split('id=')[1];//search get url params & split get path of the searched element
+    console.log(id);
+    //const photographer =  photographers.filter(el => el.id === id);
+    //return console.log(photographer); // cannot destructure property photographer
+
+    return  photographers.filter(function (photographer) { // pas d'erreur mais ne retourne pas le HTML de la factory
+            return photographer.id === id;
+        }
+    )
+}
+
+
+
+async function displayData(photographer) { //display data dynamically for each photographer
+
+    const photographersSection = document.querySelector("#main");
+
+        const profileModel = photographerFactory(photographer);
+        const profiles = profileModel.getProfilePage();
+        photographersSection.appendChild(profiles);
+
+
+};
+
+
+async function init() {
+    // Récupère les datas des photographes
+    const {photographer} = await getPhotographer();
+    await displayData(photographer);
+};
+
+init();
+//partir sur une classe avec toutes les fonctions (PhotographBuilder)
+
+let counter = 0;
+function incrementLikes()
+{
+    const hearts = document.querySelectorAll('i.fa-heart');
+    console.log(hearts);
+    hearts.addEventListener('click', function (e)
+    {
+        e.preventDefault();
+        counter++;
+     const  compteurs = document.querySelectorAll('.compteur');
+
+
+        for (const compteur of compteurs) {
+
+            compteur.innerHTML = counter;
+
+        }
+
+    });
+
+}
+
+
+
+
+
+
